281405c2bcf4f57b8cf351f5373ee77df6631c1f
yogipuji15 <yogipuji.yps@gmail.com>
2023-09-02 09:09:31 +0700

    refactor(generate-domain): Give command description

--- cmd/domain.go
+++ cmd/domain.go
@@ -19,13 +19,9 @@ type Data struct {
 
 // domainCmd represents the domain command
 var domainCmd = &cobra.Command{
-	Use:   "domain",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "domain <domain-name>",
+	Short: "Generate a new domain or entity for your REST API project.",
+	Long: `This command allows you to create a new domain or entity within your REST API project. A domain entity represents a specific resource or data structure in your API, such as User, Product, or Order. Customize the generated code according to your project's specific requirements.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) == 0 {
 			return errors.New("Specified your domain name")
