e66e45ccd5e173df81192b8b575761d38a81c98e
yogipuji15 <yogipuji.yps@gmail.com>
2023-09-02 08:47:51 +0700

    refactor(generate-infra): Refactor the return logic using RunE and fix postgresql’s type in database struct

--- cmd/infra.go
+++ cmd/infra.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"be-cli/template"
 	"be-cli/util"
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -31,7 +32,7 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error{
 		if database != "" {
 			var data Database
 			if database == "mysql" {
@@ -43,48 +44,44 @@ to quickly create a Cobra application.`,
 				}
 			} else if database == "postgresql" {
 				data = Database{
-					Type:       database,
+					Type:       "postgres",
 					DataSource: "\"postgresql://%s:%s@%s/%s?sslmode=disable\",os.Getenv(\"DB_USER\"),os.Getenv(\"DB_PASS\"),os.Getenv(\"DB_HOST\"),os.Getenv(\"DB_NAME\"),",
 					Package:    "github.com/lib/pq",
 					Command:   	"go get github.com/lib/pq",
 				}
 			} else {
-				fmt.Println("Specified your database type. ex: mysql,postgresql")
-				return
+				return errors.New("Specified your database type. ex: mysql,postgresql")
 			}
 
 			_, err := util.ExecuteCommand(data.Command)
 			if err != nil {
-				fmt.Println(err)
-				return
+				return err
 			} else {
 				fmt.Println("successed: Installing "+data.Type+" driver")
 			}
 
 			file, err := util.CreateFile("infrastructure/" + database + ".go")
 			if err != nil {
-				fmt.Println(err)
-				return
+				return err
 			} else {
 				fmt.Println("successed: Make file infrastructure/" + database + ".go")
 			}
 
 			fileString, err := template.GetFileString("file-template/sql.tmpl")
 			if err != nil {
-				fmt.Println(err)
-				return
+				return err
 			}
 
 			err = util.ExecuteTemplate(data, "sql.tmpl", fileString, file)
 			if err != nil {
-				fmt.Println(err)
-				return
+				return err
 			} else {
 				fmt.Println("successed: Create " + database + " database connection")
 			}
 		}else{
 			cmd.Help()
 		}
+		return nil
 	},
 }
 
