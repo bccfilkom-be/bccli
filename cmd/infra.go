8aacff60dc191ea96c817879d9b596e7c3c8d8e5
Naufal Haris <naufal.michiels@gmail.com>
2024-09-14 21:34:10 +0700

    refactor(domain): change run to run and change if statement to switch

--- cmd/infra.go
+++ cmd/infra.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -26,82 +23,66 @@ type Database struct {
 var infraCmd = &cobra.Command{
 	Use:   "infra [flags]",
 	Short: "Generate infra [flags] components",
-	Long: ``,
-	Run: func(cmd *cobra.Command, args []string) {
+	Long: `This command allows you to generate infrastructure components for your REST API project. You can specify the type of infrastructure you want to generate using the flag and provide the specific type of infrastructure as an argument. This command streamlines the process of creating essential infrastructure components for your REST API, such as databases, caching systems, or other dependencies. It ensures that your project has the necessary infrastructure in place to support its functionality. Customize the generated infrastructure components according to your project's specific requirements, and  use the appropriate flags and arguments to tailor the generation process.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
 
 		if cmd.Flags().NFlag() == 0 {
-			fmt.Println("error: please specified your flag. ex: --db=mysql")
-			return
+			return fmt.Errorf("please specified your flag. ex: --db=mysql")
 		}
-    
-		if database != "" {
-			var data Database
-			if database == "mysql" {
-				data = Database{
-					Type:       database,
-					DataSource: "\"%s:%s@tcp(%s)/%s\",os.Getenv(\"DB_USER\"),os.Getenv(\"DB_PASS\"),os.Getenv(\"DB_HOST\"),os.Getenv(\"DB_NAME\"),",
-					Package:    "github.com/go-sql-driver/mysql",
-					Command:   	"go get github.com/go-sql-driver/mysql",
-				}
-			} else if database == "postgresql" {
-				data = Database{
-					Type:       "postgres",
-					DataSource: "\"postgresql://%s:%s@%s/%s?sslmode=disable\",os.Getenv(\"DB_USER\"),os.Getenv(\"DB_PASS\"),os.Getenv(\"DB_HOST\"),os.Getenv(\"DB_NAME\"),",
-					Package:    "github.com/lib/pq",
-					Command:   	"go get github.com/lib/pq",
-				}
-			} else {
-				fmt.Println("error: please specified your database type. ex: --db=mysql")
-				return
-			}
 
-			_, err := util.ExecuteCommand(data.Command)
-			if err != nil {
-				fmt.Println(err)
-				return
-			} else {
-				fmt.Println("successed: Installing "+data.Type+" driver")
-			}
+		var data Database
 
-			file, err := util.CreateFile("infrastructure/" + database + ".go")
-			if err != nil {
-				fmt.Println(err)
-				return
-			} else {
-				fmt.Println("successed: Make file infrastructure/" + database + ".go")
+		switch database {
+		case "mysql":
+			data = Database{
+				Type:       database,
+				DataSource: "\"%s:%s@tcp(%s)/%s\",os.Getenv(\"DB_USER\"),os.Getenv(\"DB_PASS\"),os.Getenv(\"DB_HOST\"),os.Getenv(\"DB_NAME\"),",
+				Package:    "github.com/go-sql-driver/mysql",
+				Command:    "go get github.com/go-sql-driver/mysql",
 			}
-
-			fileString, err := template.GetFileString("file-template/sql.tmpl")
-			if err != nil {
-				fmt.Println(err)
-				return
+		case "postgresql":
+			data = Database{
+				Type:       "postgres",
+				DataSource: "\"postgresql://%s:%s@%s/%s?sslmode=disable\",os.Getenv(\"DB_USER\"),os.Getenv(\"DB_PASS\"),os.Getenv(\"DB_HOST\"),os.Getenv(\"DB_NAME\"),",
+				Package:    "github.com/lib/pq",
+				Command:    "go get github.com/lib/pq",
 			}
-
-			err = util.ExecuteTemplate(data, "sql.tmpl", fileString, file)
-			if err != nil {
-				fmt.Println(err)
-				return
-			} else {
-				fmt.Println("successed: Create " + database + " database connection")
-			}
-		}else{
-			cmd.Help()
+		default:
+			return fmt.Errorf("database type not found")
 		}
+
+		_, err := util.ExecuteCommand(data.Command)
+		if err != nil {
+			return err
+		} else {
+			fmt.Println("successed: Installing " + data.Type + " driver")
+		}
+
+		file, err := util.CreateFile("infrastructure/" + database + ".go")
+		if err != nil {
+			return err
+		} else {
+			fmt.Println("successed: Make file infrastructure/" + database + ".go")
+		}
+
+		fileString, err := template.GetFileString("file-template/sql.tmpl")
+		if err != nil {
+			return err
+		}
+
+		err = util.ExecuteTemplate(data, "sql.tmpl", fileString, file)
+		if err != nil {
+			return err
+		} else {
+			fmt.Println("successed: Create " + database + " database connection")
+		}
+
+		return nil
 	},
 }
 
 func init() {
 	infraCmd.Flags().StringVarP(&database, "db", "d", "", "Flag to generate database connection. infra-spesific-type: mysql,postgresql")
-  
+
 	rootCmd.AddCommand(infraCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// infraCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// infraCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
